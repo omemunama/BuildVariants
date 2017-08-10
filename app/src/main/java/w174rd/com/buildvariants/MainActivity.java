67acbf66bcc3281692f7887a2eaff026f525296b
najib budin <najib.budin@appschef.com>
2017-08-10 16:45:29 +0700

    Menambahkan code untuk menampilkan nama app dan menampilkan string pada file properties

--- app/src/main/java/w174rd/com/buildvariants/MainActivity.java
+++ app/src/main/java/w174rd/com/buildvariants/MainActivity.java
@@ -1,13 +1,19 @@
 package w174rd.com.buildvariants;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView apiUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        apiUrl = (TextView) findViewById(R.id.url_api);
+
+        apiUrl.setText(BuildConfig.M_API_URL);
     }
 }
